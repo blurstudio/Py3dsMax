f5f0e0eb060866050397cc43ee63d99546ef00e7
Mike <mikeh@blur.com>
2015-05-12 15:29:38 -0700

    Updates to version.h generation
    Revision is now the commit count. commit hash is now only stored in the
    version string.
    Previous commit didn't compile because rev was not a int value.

--- buildVersion.py
+++ buildVersion.py
@@ -13,16 +13,28 @@ import re
 import datetime
 from subprocess import Popen, PIPE
 
-p = Popen(['git', 'describe', '--long'], stdout=PIPE)
-gitVersion, err = p.communicate()
-if err:
-	sys.exit(err)
+def interrogateGit(arguments, regex):
+	p = Popen(arguments, stdout=PIPE)
+	ret, err = p.communicate()
+	if err:
+		sys.exit(err)
+	match = re.match(regex, ret)
+	if not match:
+		print 'Unable to find match'
+		print [ret]
+		print regex
+		sys.exit(2)
+	return match
+	
+gitVersion = interrogateGit(
+	['git', 'describe', '--long'], 
+	r'v(?P<major>\d+).(?P<minor>\d+).(?P<patch>\d+)-\d+-(?P<rev>[\w]+)'
+)
 
-match = re.match(r'v(?P<major>\d+).(?P<minor>\d+).(?P<patch>\d+)-\d+-(?P<rev>[\w]+)', gitVersion)
-if not match:
-	sys.exit(2)
+data = gitVersion.groupdict()
 
-data = match.groupdict()
+revCount = interrogateGit(['git', 'rev-list', 'HEAD', '--count'], r'\d+')
+data['revCount'] = revCount.group(0)
 
 time = datetime.datetime.now()
 data['datetime'] = time.strftime('%Y/%m/%d %H:%M:%S')
